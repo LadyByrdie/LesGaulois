c805ba0ffe74ef2a6c568b8483fed91d52bcfe4c
LadyByrdie <114427314+LadyByrdie@users.noreply.github.com>
2022-10-05 15:29:34 +0200

    le main est crée

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -1,5 +1,51 @@
 package personnages;
 
 public class Village {
+	private String nom;
+	private Chef chef;
+	private int nbVillageois = 0;
+	private Gaulois[] villageois;
+	int nbVillageoisMaximum;
+	
+	public void setChef(Chef chef) {
+		this.chef = chef;
+	}
 
+	public String getNom() {
+		return nom;
+	}
+	
+	public Village(String nom, int nbVillageoisMaximum) {
+		this.nom = nom;
+		this.nbVillageoisMaximum = nbVillageoisMaximum;
+		this.villageois = new Gaulois[nbVillageoisMaximum];
+		
+	}
+
+	public void ajouterHabitant(Gaulois gaulois) {
+		if (nbVillageois < nbVillageoisMaximum) {
+			villageois[nbVillageois]= gaulois;
+			nbVillageois ++;
+		} else {
+			System.out.println("Village complet");
+		}
+	}
+	
+	public void trouverHabitant(int number) {
+		if (nbVillageois > 0) {
+			System.out.println(villageois[number]);
+		} else {
+			System.out.println("Le village est vide");
+		}
+		System.out.println(villageois[number]);
+	}
+	
+	public static void main(String[] args) {
+		Village village;
+		village= new Village("Village des Irréductibles",30);
+		
+	}
+	
+	
+	
 }
